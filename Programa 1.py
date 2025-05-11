b47d3938a892aa73c04554e423b81d6324183f5e
RibeiroIF <Gabriel.souza.0702@gmail.com>
2025-05-11 12:23:29 -0300

    Add files via upload

---
+++ Programa 1.py
@@ -0,0 +1,17 @@
+x = str ("Guess who's finally getting his powers. ")
+y = str ("Are you sure? ")
+z = str("Pretty sure.")
+
+def myfunc():
+    y = str ("That's great son.")
+    print(x + y)
+    
+myfunc()
+
+print (x)
+print (type(x))
+print (y)
+print (type(y))
+print (z)
+print (type(z))
+
